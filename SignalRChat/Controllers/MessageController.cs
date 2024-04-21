9a31d9f5573d43009fc9edca92ea457c64eb8eea
ertugrullozer <ertugrulozer19944@gmail.com>
2024-04-21 15:50:29 +0300

    Proje dosyası ekle.

---
+++ SignalRChat/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
